3c9ba0ec248ac0cf9a803c737bb9e3800152af49
Nurul Syafika <nrlsyfika30@gmail.com>
2026-09-23 14:18:33 +0700

    Pertemuan 2 latihan PHP dasar

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,29 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/latihan-php', function () {
+    $nama = 'Nurul Syafika';
+    $nilai = [88, 87, 78, 90, 96];
+
+    $hitungRataRata = function (array $data): float {
+        $total = 0;
+
+        foreach ($data as $angka) {
+            $total += $angka;
+        }
+
+        return $total / count($data);
+    };
+
+$rataRata = $hitungRataRata($nilai);
+
+    if ($rataRata >= 75) {
+        $status = 'Lulus';
+    } else {
+        $status = 'Perlu Perbaikan';
+    }
+
+    return view('latihan-php', compact(
+        'nama', 'nilai', 'rataRata', 'status'
+    ));
+});
